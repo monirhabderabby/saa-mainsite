732f196bb166bcd9eaae945667fdccfafc7a8467
Monir Hossain <monirhrabby.personal@gmail.com>
2025-09-11 10:21:00 +0600

    a

---
+++ src/app/(auth)/registration/verification/[token]/page.tsx
@@ -0,0 +1,34 @@
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+
+const Page = ({}: { params: { token: string } }) => {
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <Card className="min-w-[300px] shadow-none">
+        <CardHeader>
+          <div></div>
+          <CardTitle className="text-center">Verification Completed</CardTitle>
+          <CardDescription className="text-center">
+            Congratulations, your identity has been verified.
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="flex justify-center items-center">
+          <Image
+            src="/black-logo.svg"
+            width={120}
+            height={47}
+            alt="black logo"
+          />
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/components/checkIcon.tsx
@@ -0,0 +1,25 @@
+// function CheckIcon(props: any) {
+//   return (
+//     <svg
+//       {...props}
+//       fill="none"
+//       viewBox="0 0 24 24"
+//       stroke="currentColor"
+//       strokeWidth={3}
+//     >
+//       <motion.path
+//         initial={{ pathLength: 0 }}
+//         animate={{ pathLength: 1 }}
+//         transition={{
+//           delay: props?.delay || 0.2,
+//           type: "tween",
+//           ease: "easeOut",
+//           duration: 0.3,
+//         }}
+//         strokeLinecap="round"
+//         strokeLinejoin="round"
+//         d="M5 13l4 4L19 7"
+//       />
+//     </svg>
+//   );
+// }
