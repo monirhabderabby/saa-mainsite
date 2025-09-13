6de9d7db6f98f7fdfd545c06875a0b1bf418aa35
unknown <monirhrabby.personal@gmail.com>
2025-09-13 06:50:42 +0600

    developed: some column on update entry table

--- src/app/(dashboard)/update-sheet/_components/tables/update-sheet-columns.tsx
+++ src/app/(dashboard)/update-sheet/_components/tables/update-sheet-columns.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { UpdateSheetData } from "@/helper/update-sheet";
 import { ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
 
 export const updateSheetColumns: ColumnDef<UpdateSheetData>[] = [
   {
@@ -19,7 +21,48 @@ export const updateSheetColumns: ColumnDef<UpdateSheetData>[] = [
     accessorKey: "updateBy",
     header: "Update By",
     cell: ({ row }) => {
-      return <div>{row.original.updateBy.fullName}</div>;
+      return (
+        <div className="flex justify-start items-center gap-x-2">
+          {" "}
+          <Image
+            src="/placeholder.avif"
+            height={20}
+            width={20}
+            alt={row.original.updateBy.fullName as string}
+          />
+          {row.original.updateBy.fullName}
+        </div>
+      );
     },
+  },
+  {
+    accessorKey: "message",
+    header: "Message",
+    cell: ({}) => (
+      <Button variant="outline" size="sm">
+        View
+      </Button>
+    ),
+  },
+  {
+    accessorKey: "attachments",
+    header: "Attachments",
+    cell: ({ row }) => (
+      <a
+        href={row.original.attachments as string}
+        className="hover:text-blue-500 transition-colors duration-300"
+        target="_blank"
+      >
+        {row.original.attachments}
+      </a>
+    ),
+  },
+  {
+    accessorKey: "commentFromOperation",
+    header: "Comment (Operation)",
+  },
+  {
+    accessorKey: "commentFromSales",
+    header: "Comment (Sales)",
   },
 ];
