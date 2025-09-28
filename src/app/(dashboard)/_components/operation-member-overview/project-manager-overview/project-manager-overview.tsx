755e726f1b783a2c7d1cba5f45605c7893e35386
unknown <monirhrabby.personal@gmail.com>
2025-09-28 21:16:29 +0600

    a

--- src/app/(dashboard)/_components/operation-member-overview/project-manager-overview/project-manager-overview.tsx
+++ src/app/(dashboard)/_components/operation-member-overview/project-manager-overview/project-manager-overview.tsx
@@ -13,7 +13,7 @@ const ProjectManagerOverview = async () => {
   const cu = await auth();
   if (!cu || !cu.user || !cu.user.id) redirect("/login");
 
-  const service = await prisma.services.findFirst({
+  const services = await prisma.services.findMany({
     where: {
       serviceManagerId: cu.user.id,
     },
@@ -27,14 +27,23 @@ const ProjectManagerOverview = async () => {
     },
   });
 
-  if (!service) notFound();
+  if (!services || services.length === 0) {
+    notFound();
+  }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-      {service.teams.map((team) => (
-        <Suspense fallback={<CardSkeleton />} key={team.id}>
-          <ProjectManagerOverviewTeam teamId={team.id} teamName={team.name} />
-        </Suspense>
+    <div className="grid gap-5">
+      {services.map((service) => (
+        <div key={service.id} className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          {service.teams.map((team) => (
+            <Suspense fallback={<CardSkeleton />} key={team.id}>
+              <ProjectManagerOverviewTeam
+                teamId={team.id}
+                teamName={team.name}
+              />
+            </Suspense>
+          ))}
+        </div>
       ))}
     </div>
   );
