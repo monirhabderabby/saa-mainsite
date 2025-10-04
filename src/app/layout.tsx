179fd59c9b305477fc3df5769e6248d7235ac139
Monir Hossain <monirhrabby.personal@gmail.com>
2025-10-04 11:23:42 +0600

    added: patch 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export default async function RootLayout({
             <TanstackProvider>
               <EdgeStoreProvider>{children}</EdgeStoreProvider>
               <p className="absolute bottom-2 right-7 text-[12px]">
-                Beta v1.0.2
+                Beta v1.0.3
               </p>
             </TanstackProvider>
           </ThemeProvider>{" "}
