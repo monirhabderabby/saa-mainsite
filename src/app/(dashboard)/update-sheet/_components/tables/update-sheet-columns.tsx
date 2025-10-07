db44e845eb734a1f104573d0c022aa61589f6cbf
unknown <monirhrabby.personal@gmail.com>
2025-10-07 20:13:32 +0600

    refactor: code

--- src/app/(dashboard)/update-sheet/_components/tables/update-sheet-columns.tsx
+++ src/app/(dashboard)/update-sheet/_components/tables/update-sheet-columns.tsx
@@ -6,13 +6,21 @@ import { Pencil } from "lucide-react";
 import moment from "moment";
 import dynamic from "next/dynamic";
 import Link from "next/link";
-import CommentFormOperation from "./comment-from-operation";
-import DoneByComponent from "./done-by-component";
+import CommentFormOperation from "./columns/comment-from-operation";
+import DoneByComponent from "./columns/done-by-component";
+import TlCheckComponent from "./columns/tl-check-component";
 import { CurrentUserTeam } from "./table-container";
-import TlCheckComponent from "./tl-check-component";
-import UpdateToComponents from "./update-to";
-import UpdatedByComponents from "./updated-by-component";
-const ViewUpdateSheetModal = dynamic(() => import("./view-modal"), {
+const UpdateToComponents = dynamic(() => import("./columns/update-to"), {
+  ssr: false,
+});
+const UpdatedByComponents = dynamic(
+  () => import("./columns/updated-by-component"),
+  {
+    ssr: false,
+  }
+);
+
+const ViewUpdateSheetModal = dynamic(() => import("./columns/view-modal"), {
   ssr: false,
 });
 
