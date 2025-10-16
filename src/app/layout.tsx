aeee9531854ef22f1a2200e9f1e28482e5a08934
Monir Hossain Rabby <96588176+monirhabderabby@users.noreply.github.com>
2025-10-16 14:29:17 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export default async function RootLayout({
             <TanstackProvider>
               <EdgeStoreProvider>{children}</EdgeStoreProvider>
               <p className="absolute bottom-2 right-7 text-[12px]">
-                Beta v1.0.6
+                Beta v1.0.7
               </p>
             </TanstackProvider>
           </ThemeProvider>{" "}
